651d742c3ae81e1e350a7d6147cb62edb0565701
Gabriele Piccini <46595230+g4bri-3l3@users.noreply.github.com>
2026-08-27 13:26:49 +0200

    Update MooseAlto.ps1

--- MooseAlto.ps1
+++ MooseAlto.ps1
@@ -119,7 +119,7 @@ if (-not $OutCsv) { $OutCsv = "report_$defaultTimestamp.csv" }
 # Banner. Always shown, whether or not parameters were supplied.
 # --------------------------------------------------------------------------
 
-$script:MooseAltoVersion = "1.3"
+$script:MooseAltoVersion = "1.4"
 
 function Show-Banner {
     $lines = @(
